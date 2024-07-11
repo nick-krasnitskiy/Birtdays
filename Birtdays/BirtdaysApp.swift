b497e2319e5e1376702d97abbc8960ba91a59f6e
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2024-07-11 15:26:15 +0300

    Initial Commit

---
+++ Birtdays/BirtdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirtdaysApp.swift
+//  Birtdays
+//
+//  Created by Nick Krasnitskiy on 11.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct BirtdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Birtdays/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Birtdays
+//
+//  Created by Nick Krasnitskiy on 11.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
